551f5d60314a45f0715c83cb48521dafb6f312e8
Explorer-Machine <849558437@qq.com>
2026-01-15 20:28:22 +0800

    fix(problem): fix return result error

--- problem-service/src/main/java/org/example/oj/problemService/controller/ProblemController.java
+++ problem-service/src/main/java/org/example/oj/problemService/controller/ProblemController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/problem")
 public class ProblemController {
@@ -13,7 +15,7 @@ public class ProblemController {
     ProblemService problemService;
 
     @GetMapping("/problemList")
-    public String getProblemList(){
+    public List<String> getProblemList(){
         return problemService.getProblemTitleList();
     }
 }
