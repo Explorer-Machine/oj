503f518e0ed8165864e2c0792c2f3a551367723b
Explorer-Machine <849558437@qq.com>
2026-01-15 20:20:25 +0800

    fix(problem): access service

--- problem-service/src/main/java/org/example/oj/problemService/controller/ProblemController.java
+++ problem-service/src/main/java/org/example/oj/problemService/controller/ProblemController.java
@@ -1,5 +1,7 @@
 package org.example.oj.problemService.controller;
 
+import org.example.oj.problemService.service.ProblemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/problem")
 public class ProblemController {
+    @Autowired
+    ProblemService problemService;
 
     @GetMapping("/problemList")
     public String getProblemList(){
-        return "some problem list";
+        return problemService.getProblemTitleList();
     }
 }
